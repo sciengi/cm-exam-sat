5e78068c25d021b6ade86d385f56b0e640f4bd7e
sciengi <notpiterovalexander@gmail.com>
2026-05-05 19:09:48 +0300

    feat(CNF): full CNF interface, parser for strict DIMACS fmt

--- src/cnf/cnf.cpp
+++ src/cnf/cnf.cpp
@@ -1,10 +1,95 @@
 #include <cnf/cnf.hpp>
 
-#include <algorithm>
+#include <fstream>
 
 
-CNF::CNF([[maybe_unused]] std::filesystem::path dimacscnf_file) { }
 
-size_t CNF::variable_count() const { return 10; }
+CNF::CNF(std::filesystem::path dimacscnf_file) {
 
-bool CNF::operator()(const CNF::model& model) { return std::count(model.begin(), model.end(), true) > 5; }
+    std::ifstream file(dimacscnf_file);
+    if (!file.is_open()) {
+        throw std::runtime_error("Cant open a file");
+    }
+
+    // DEV: suppose DIMACS format as 
+    // [c comment body]* 
+    // p cnf [var_count] [cl_count]
+    // [clauses]+
+    //
+    // - No comments between clauses and after task
+
+    char cmd;
+    while (file >> cmd && cmd == 'c') {
+        file.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    }
+
+    // DEV: stream can crush, so check file.fail(), ...
+
+
+    if (file.eof() || cmd != 'p') { 
+        throw std::runtime_error("No task found");
+    }
+    
+    // assert(cmd == 'p'); 
+
+    file.ignore(5); // _cnf_
+    file >> m_variable_count >> m_clause_count;
+    file.ignore();
+
+
+    m_data_size = var_in_clause * m_clause_count;
+    m_data = std::make_unique<int[]>(m_data_size);
+    
+    int literal_index;
+    size_t i = 0;
+
+    while (file >> literal_index && i < m_data_size) {
+        if (literal_index == 0) {
+            continue; // TODO: add mixed task checking 
+        }
+
+        m_data[i++] = literal_index;
+    }
+
+    if (i != m_data_size) {
+        throw std::runtime_error("Parse error");
+    }
+
+    // DEV: can check rest of a file
+   
+    /*
+    std::cout << m_variable_count << m_clause_count << std::endl; 
+    for (size_t j = 0; j < m_data_size; j++)
+        std::cout << m_data[j] << std::endl;
+    */
+}
+
+
+size_t CNF::variable_count() const { return m_variable_count; }
+
+
+size_t CNF::clause_count() const { return m_clause_count; }
+
+
+std::span<const int, CNF::var_in_clause> CNF::operator[](int i) {
+    return std::span<const int, var_in_clause>{m_data.get() + i * var_in_clause, var_in_clause}; 
+}
+
+
+bool CNF::operator()(const CNF::model& model) {
+  
+    bool cl_result;
+    for (size_t i = 0; i < clause_count(); i++) {
+        const auto& cl = (*this)[i];
+        cl_result = false;
+        for (size_t q = 0; q < var_in_clause; q++) {
+            const auto& index = cl[q];
+            cl_result = cl_result || (index > 0 ? model[index - 1] : !model[-index - 1]);
+        }
+
+        if (!cl_result) { return false; }
+    }
+
+    return true;
+}
+
--- src/cnf/cnf.hpp
+++ src/cnf/cnf.hpp
@@ -3,18 +3,32 @@
 
 #include <filesystem>
 #include <vector>
+#include <memory>
+#include <span>
 
 
 
 class CNF {
     public:
-        using model = std::vector<bool>;
+        using model = std::vector<bool>;  // DEV: make std::span?
+        static const size_t var_in_clause = 3;
 
+    public:
         CNF(std::filesystem::path dimacscnf_file);
 
         size_t variable_count() const;
 
+        size_t clause_count() const;
+
+        std::span<const int, CNF::var_in_clause> operator[](int i);
+
         bool operator()(const model& model);
+
+    private:
+        size_t m_variable_count;
+        size_t m_clause_count;
+        size_t m_data_size;
+        std::unique_ptr<int[]> m_data;
 };
 
 #endif
